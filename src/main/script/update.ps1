974abc7db3bf4751299666d2eb08c7ebb3c927b2
Bernd Eilers <bernd.eilers@fms-logistics.com>
2019-02-27 08:07:49 +0100

    add sample scripts

---
+++ src/main/script/update.ps1
@@ -0,0 +1,201 @@
+﻿
+param (
+    [Parameter(Mandatory=$true)][string]$server,
+    [Parameter(Mandatory=$true)][string]$port,
+    [switch]$install = $false,
+    [switch]$wait = $false,
+    [switch]$help = $false,
+    [Parameter(ValueFromRemainingArguments)][System.Collections.Arraylist]$launcherArguments
+)
+
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+Add-Type -AssemblyName PresentationFramework
+
+[string]$app = "sample"
+[string]$appname = "sample"
+[string]$icon = "sample.ico"
+[string]$mainClass = "myproject.Sample"
+
+[string]$os = "windows"
+[string]$Dest = $($HOME) + "\" + $($app) + "-runtime";
+write-output "Dest=$($Dest)";
+
+[string]$BaseUrl = "http://$($server):$($port)/$($app)/update/bootstrap/$($os)";
+  
+[string]$ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
+ 
+function usage
+{
+    write-output ""
+    write-output "usage:"
+    write-output "update.ps1 -server <server> -port <port> [<additionalLauncherArguments>] [-install] [-wait] [-h]"
+    Exit
+}
+
+function error_message
+{
+    param([string]$msg)
+    [System.Windows.MessageBox]::Show($($msg), 'Error!', 'OK', 'ERROR')
+}
+
+function fail_and_exit
+{
+    param([string]$msg)
+    write-output $($msg)
+    error_message $($msg)
+    Exit
+}
+
+if ( $help ) {
+    usage
+}
+
+function Unzip
+{
+    param([string]$zipfile, [string]$outpath)
+    $t = Test-Path $outpath;
+    if ( $t ) {
+        rm -r -fo $($outpath);
+    }
+
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+#   Start-Process -FilePath "$($ScriptDir)\unzip.exe" -ArgumentList "-q -d $($outpath) $($zipfile)" -WindowStyle Hidden -PassThru
+}
+
+function Update
+{
+       
+
+write-output "BaseURL=$($BaseUrl)";
+
+
+[string]$ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
+
+[string]$url=$($BaseUrl)+"/"+"update.sha256";
+[string]$output=$($ScriptDir)+"\"+"update.sha256";
+ 
+$wc = New-Object System.Net.WebClient
+write-output "downloading $($url) to $($output)";
+ 
+ Try {
+ $wc.DownloadFile($($url), $($output))
+} Catch {
+    fail_and_exit "Error downloading file $($url)";
+}
+
+$sha = Get-Content -Path $output
+
+$mustUpdate = $true;
+
+$Check="$($Dest)\update.sha256";
+
+$e=Test-Path $Check;
+
+if ( $e ) {
+    write-output "Checking ${$Check$}"
+    $sha2 = Get-Content -Path $Check
+    if ( $($sha2) -eq $($sha) ) {
+        $mustUpdate=$false;
+    }
+}
+
+
+if ( $mustUpdate ) {
+
+    write-output "Runtime Update required"
+
+[string]$url=$($BaseUrl)+"/"+"update.zip";
+[string]$output2=$($ScriptDir)+"\"+"update.zip";
+
+write-output "downloading $($url) to $($output2)";
+Try {
+ $wc.DownloadFile($($url), $($output2))
+} Catch {
+    fail_and_exit "Error downloading file $($url)";
+}
+
+$hashFromFile = Get-FileHash $($output2)
+
+if ( $hashFromFile.hash -ne $sha ) {
+
+    rm -r -fo $($output)
+    rm -r -fo $($output2)
+
+    fail_and_exit "hash values differ\r\nexpected: $($sha)\r\n     got: $($hashFromFile.hash)";
+} else {
+    write-output "SHA Hash Values matches: $sha"; 
+}
+
+Unzip $($output2) $($Dest)
+
+cp $($output) $($Check)
+
+rm -r -fo $($output)
+rm -r -fo $($output2)
+
+} else {
+
+    write-output "No Runtime Update required"
+
+    rm -r -fo $($output)
+
+
+}
+}
+
+function Install
+{
+[string]$ScriptName=$script:MyInvocation.MyCommand.Name
+[string]$Starter="$($ScriptDir)\${ScriptName}";
+
+[string]$Desktop=[Environment]::GetFolderPath("Desktop")
+
+write-output "Starter=$($Starter)"
+write-output "Desktop=$($Desktop)"
+	
+$Shell = New-Object -ComObject ("WScript.Shell")
+[string]$lnkname="$($appname)"
+
+$ShortCut = $Shell.CreateShortcut("$($Desktop)\$($lnkname).lnk")
+
+$ShortCut.TargetPath="%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe"
+$q=""
+foreach ($arg in $launcherArguments ) {
+	if ( $q -eq "" ) {
+	   $q="`"$($arg)`""
+	} else {
+	   $q="$($q) `"$($arg)`""
+	}
+}
+
+$ShortCut.Arguments="-WindowStyle Hidden -ExecutionPolicy Bypass -File $($ScriptDir)\$($ScriptName) `"-server`" `"$($server)`" `"-port`" `"$($port)`"  $($q)";
+
+$ShortCut.WorkingDirectory = "$($Dest2)";
+$ShortCut.WindowStyle = 1;
+$ShortCut.IconLocation = "$($ScriptDir)\$($icon)";
+$ShortCut.Description = "$($appname)";
+$ShortCut.Save()
+}
+
+
+function Launch
+{
+    cd $($Dest)
+
+    Start-Process -FilePath bin\java -ArgumentList "--module-path app\jar_auto -splash:conf\splash.gif $(mainClass) $($launcherArguments)" -WindowStyle Hidden -PassThru 
+}
+
+
+if ( $install ) {
+Install
+} else {
+Update
+Launch
+}
+
+
+if ( $wait ) {
+$( Read-Host "Type anything to quit" );
+}
+
+
---
+++ src/main/script/update.sh
@@ -0,0 +1,212 @@
+#!/bin/bash
+
+app="sample"
+icon="sample.ico"
+mainClass="myproject.Sample"
+
+os=`uname`
+if [ "${os}" == "Darwin" ]; then
+os="mac"
+elif [ "${os} == "Linux ]; then
+os="linux"
+fi
+
+usage() {
+    echo
+    echo "usage:"
+    echo "update.sh -server <server> -port <port> [<additionalLauncherArguments>] [-install] [-wait] [-h]"
+    exit 0;
+}
+
+zenity_msg () {
+	which zenity > /dev/null 2>&1
+	if [ $? -eq 0 ]; then
+		zenity --$1 --text "$2"
+	fi
+}
+
+fail_and_exit () {
+	# args $1 failure message
+	echo "$1"
+	zenity_msg "error" "$1"
+	exit 1
+}
+
+wait="false"
+install="false"
+
+function quote() {
+   arr=("$@")
+   q=""
+   for i in "${arr[@]}";
+      do
+          if [ "${q}" == "" ]; then
+              q="\"$i\""
+	  else
+              q="${q} \"$i\""
+	  fi
+     done
+   echo $q
+}
+
+declare -a launcherArguments
+
+until [ -z "$1" ]; do
+  case $1 in
+      "-h")
+	  shift
+	  usage
+      ;;
+      "-server")
+	  shift
+	  if [ $# -eq 0 ]; then
+	      usage
+	  fi
+	  server=$1
+	  shift
+      ;;
+      "-port")
+	  shift
+	  if [ $# -eq 0 ]; then
+	      usage
+	  fi
+	  port=$1
+	  shift
+      ;;
+      "-install")
+	  shift
+	  install="true"
+      ;;
+      "-wait")
+	  shift
+	  wait="true"
+      ;;
+      *)
+	  launcherArguments+=$1;
+	  shift
+  esac
+done    
+
+if [ "$server" == "" ]; then
+    echo -n "server: "
+    read server
+fi
+if [ "$port" == "" ]; then
+    echo -n "port: "
+    read port
+fi
+
+
+Dest="${HOME}/${app}-runtime"
+
+ScriptDir=$(readlink -f "$0")
+ScriptName=$(basename "${ScriptDir}")
+ScriptDir=$(dirname "${ScriptDir}") 
+
+Update() {
+BaseUrl="http://${server}:${port}/${app}/update/bootstrap/${os}"
+
+echo "BaseUrl=${BaseUrl}"
+
+cd "${ScriptDir}"
+
+output="${ScriptDir}/update.sha256"
+
+url="${BaseUrl}/update.sha256"
+curl -f -s $url -o "${output}"
+if [ $? -ne 0 ]; then
+    fail_and_exit "error downloading ${url}"
+fi
+
+sha=`cat update.sha256`
+mustUpdate="true" 
+
+Check="${Dest}/update.sha256"
+
+if [ -f "${Check}" ]; then
+    sha2=`cat "${Check}"`
+    if [ "${sha}" == "${sha2}" ]; then
+	mustUpdate="false"
+    else
+	echo "hash values differ";
+	echo "expected: $sha";
+	echo "     got: $sha2";
+    fi
+else 
+    echo "checksum file not present ${Check}"
+fi
+
+if [ ${mustUpdate} == "true" ]; then
+    echo "Runtime Update required"
+    url="${BaseUrl}/update.zip"
+    output2="${ScriptDir}/update.zip"
+    echo "downloading ${url}"
+    curl -f -s $url -o "${output2}"
+     if [ $? -ne 0 ]; then
+	 fail_and_exit "error downloading ${url}"
+     fi
+     sha2=`sha256sum "${output2}" | awk '{print $1}'`
+    if [ "${sha}" != "${sha2}" ]; then
+	rm -f ${output2}
+	rm -f ${output}
+	fail_and_exit "hash values differ\nexpected: $sha\n     got: $sha2";
+    else
+	echo "SHA Hash Values matches: $sha"; 
+    fi
+
+    rm -rf "${Dest}"
+    unzip -q "${output2}" -d "${Dest}"
+
+    if [ $? -ne 0 ]; then
+	rm -f ${output2}
+	rm -f ${output}
+	fail_and_exit "error unzipping ${output2}"
+    fi
+
+    cp "${output}" "${Check}"
+
+    rm -f ${output2}
+    rm -f ${output}
+
+else
+    echo "No Runtime Update required"
+    rm -f ${output}
+fi
+
+
+}
+
+Launch() {
+    cd $Dest
+    bin/java --module-path app/jar_auto -splash:conf/splash.gif ${mainClass} ${launcherArguments}
+}
+
+Install() {
+    if [ "${os}" == "mac" ]; then
+         fail_and_exit "Install not implemented for Mac"
+    fi
+    q=$(quote ${launcherArguments})
+
+    cat <<EOF >"${HOME}/Desktop/${app}.desktop"
+[Desktop Entry]
+    Exec="${ScriptDir}/${ScriptName}" -server "$server" -port "${port}" ${q}
+    Encoding=UTF-8
+    Icon=${ScriptDir}/${icon}
+    Name=${app}
+    Path=${ScriptDir}
+    Type=Application
+EOF
+
+}
+
+if [ "${install}" == "true" ]; then
+    Install
+else
+    Update
+    Launch
+fi
+
+if [ "${wait}" == "true" ]; then
+    echo -n "Type return to quit! "
+    read a
+fi
